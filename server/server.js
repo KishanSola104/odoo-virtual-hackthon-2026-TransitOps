36ba58e18f071ec340a49cabbd7ca97ff38c2945
Vansh Patel <pvansh2804@gmail.com>
2026-07-12 10:53:42 +0530

    Database created and connected

--- server/server.js
+++ server/server.js
@@ -1,21 +1,20 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const app = express();
-// Global Middlewares MUST come first
+require("dotenv").config();
 
-app.use(cors());
+const express = require("express");
+const app = express();
+
+const initializeDatabase = require("./config/initializeDatabase");
+
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Server is running');
+app.get("/", (req, res) => {
+    res.send("Welcome");
 });
 
-// Fallback Test Route
-app.get('/test', (req, res) => res.json({ status: "API is online" }));
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT} http://localhost:${PORT}`);
+app.listen(PORT, async () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+    await initializeDatabase();
 });
+
